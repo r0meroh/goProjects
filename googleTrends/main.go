ab8dfda2663f23e92c94b675bcb6bd5fe7c45f72
r0meroh <romeroh862@gmail.com>
2023-01-29 23:02:56 -0600

    finished printing trends out to console

--- googleTrends/main.go
+++ googleTrends/main.go
@@ -5,34 +5,48 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
 type RSS struct {
-	XMLName xml.Name `xml: "rss"`
-	Channel *Channel `xml: "channel"`
+	XMLName xml.Name `xml:"rss"`
+	Channel *Channel `xml:"channel"`
 }
 
 type Channel struct {
-	Title    string `xml: "title"`
-	ItemList []Item `xml: "item"`
+	Title    string `xml:"title"`
+	ItemList []Item `xml:"item"`
 }
 type Item struct {
-	Title     string `xml: "title"`
-	Link      string `xml: "link"`
-	Traffic   string `xml: "approx_traffic"`
-	NewsItems []News `xml: "news_item"`
+	Title     string `xml:"title"`
+	Link      string `xml:"link"`
+	Traffic   string `xml:"approx_traffic"`
+	NewsItems []News `xml:"news_item"`
 }
 
 type News struct {
-	Headline     string `xml: "news_item_title"`
-	HeadlineLink string `xml: "news_item_url"`
+	Headline     string `xml:"news_item_title"`
+	HeadlineLink string `xml:"news_item_url"`
 }
 
 func main() {
 	fmt.Println("hello")
 	var r RSS
 	data := readGoogleTrends()
+	err := xml.Unmarshal(data, &r)
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	for i := range r.Channel.ItemList {
+		rank := (i + 1)
+		fmt.Println("#", rank)
+		fmt.Println("Search Term: ", r.Channel.ItemList[i].Title)
+		fmt.Println("Link to the Trend: ", r.Channel.ItemList[i].Link)
+		fmt.Println("Headline: ", r.Channel.ItemList[i].NewsItems[0].Headline)
+		fmt.Println("Link to article: ", r.Channel.ItemList[i].NewsItems[0].HeadlineLink)
+		fmt.Println("------------------------------------------")
+	}
+
 }
 
 func getGoogleTrends() *http.Response{
@@ -44,7 +58,12 @@ func getGoogleTrends() *http.Response{
 }
 
 func readGoogleTrends()[]byte{
-	getGoogleTrends()
+	resp := getGoogleTrends()
+	data, err := ioutil.ReadAll((resp.Body))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return data
 }
 
 
